d9a4322b0bf1a3ec309a826f8fb71eadc9283343
Milton <miltonio94@gmail.com>
2024-02-17 15:22:39 +0000

    Implement array and it's inbuilt funcitons

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,7 +29,7 @@ if (5 < 10){
 
 "foobar"
 "foo bar"
-
+[1, 2];
 `
 
 	tests := []struct {
@@ -120,6 +120,13 @@ if (5 < 10){
 
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
 	}
